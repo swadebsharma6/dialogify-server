62525f029ea4cf52ca8a757b0d1126815b189229
swadebsharma6 <swadebriva2012@gmail.com>
2024-01-26 18:34:19 +0600

    upto date my server side

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const categoryCollection = client.db("DialogifyDB").collection("categories");
     const allCategoriesCollection = client.db("DialogifyDB").collection("allcategoriesdata");
@@ -51,8 +51,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
